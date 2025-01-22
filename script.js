f99654932b727d667cea2b11fdcace20e12cd317
Mafer <vegapachecofer@gmail.com>
2025-01-22 11:11:10 -0500

    cards_pokemon

--- script.js
+++ script.js
@@ -2,29 +2,33 @@ document.addEventListener("DOMContentLoaded", () => {
     const link = "https://pokeapi.co/api/v2/pokemon/";
     const cards = document.querySelector("#card");
 
-    for (let i = 1; i <= 5; i++) {
+    for (let i = 1; i <= 20; i++) {
         fetch(link + i)
             .then((res) => res.json())
             .then((data) => look(data))
             .catch((error) => console.error("Error:", error));
     }
 
-    function look(data) {
+    function look(info) {
+        let tipos = info.types.map((type)=>
+            `<p class="tipo">tipo:${type.type.name}</p>`);
+            tipos= tipos.join("")      
         const div = document.createElement("div");
         div.classList.add("card");
 
         div.innerHTML = `
-            <p class="poke_name">N: ${data.id}</p>
+            <p class="poke_name">N: ${info.id}</p>
             <div class="poke_ima">
-                <img src="${data.sprites.front_default}" alt="${data.name}" />
+                <img src="${info.sprites.front_default}" alt="${info.name}" />
             </div>
             <div class="poke_info">
-                <h2 class="poke_name">${data.name}</h2>
-                <p class=""class_poke>tipo: </p>
-
+                <h2 class="poke_name">${info.name}</h2>
+                ${tipos}
+                <p class="species">especie: </p>
+                <p class="weight">peso: ${info.weight}kg</p>
+                <p class="abilities">habilidades:  </p>
             </div>
         `;
-
         cards.append(div);
     }
 });
